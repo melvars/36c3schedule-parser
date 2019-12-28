f62a6417e1c8b791efa249b1984346726c0bacbd
pixelexplosion <christopher.alan.w@gmail.com>
2019-12-28 17:19:28 +0100

    code:)

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 import requests
+import config
+
+r = requests.get(config.URL)
+
+days = r.json()[""]
+print(r.json())
--- epaper/main.py
+++ epaper/main.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import epd2in13
+import json
 import time
 import requests
 from datetime import date
@@ -15,20 +16,23 @@ epd.init(epd.FULL_UPDATE)
 epd.Clear(0xFF)
 
 image = Image.new('1', (epd2in13.EPD_HEIGHT, epd2in13.EPD_WIDTH), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(image)    
+draw = ImageDraw.Draw(image)
 font = ImageFont.truetype('/usr/share/fonts/truetype/wqy/wqy-microhei.ttc', font_size)
 
-resp = requests.get('http://data.c3voc.de/36C3/everything.schedule.json')
-days = resp.json()['schedule']['conference']['days']
+resp = json.load(open("everything.schedule.json"))
+print("marvin ist nicht toll")
+days = resp['schedule']['conference']['days']
 conferences = []
 for day in days:
     for room in day['rooms']:
         for conference in day['rooms'][room]:
-            conf_date = time.mktime(datetime.strptime(conference['date'], '%Y-%m-%dT%H:%M:%S%z').timetuple())
-            if conf_date - time.time() > 2 * 60 * 60:
+            conf_date = datetime.strptime(conference['date'].split('+')[0], '%Y-%m-%dT%H:%M:%S')
+            offset = conf_date - datetime.now()
+            if offset.seconds / 3600 < 3 and offset.seconds/3600 > 0 and offset.days==0:
+                print(offset.seconds / 3600)
                 conferences.append(conference)
-
-sorted_list = sorted(conferences, key = lambda i: i['date'])
+sorted_list = sorted(conferences, key=lambda i: i['date'])
+print(sorted_list)
 
 for i in range(1, 6):
     draw.text((0, font_size * i + 2), conferences[i - 1]['title'], font=font, fill=0)
